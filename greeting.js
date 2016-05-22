3a741b1a02a786a9864e9dfe7b7527087f4c6090
Matt Osteen <themosteen@gmail.com>
2016-05-22 14:42:47 +0900

    This is just one of the FCC examples

--- greeting.js
+++ greeting.js
@@ -4,3 +4,20 @@ function reverse(str) {
 }
 
 reverse("butts")
+
+var butts = "My Butt!"
+
+function whatAmISittingOn () {
+	return butts;
+}
+
+	whatAmISittingOn();
+	
+	function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  var result = "";
+  // Your code below this line
+  result = "The " + myAdjective + " " + myNoun + " " + myAdverb + " " + myVerb + " towards the butt.";
+
+  // Your code above this line
+  return result;
+}
